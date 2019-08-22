1d33c0c0a85dc0fe5dce1bd6d50134626d8c1fa7
BlacherD <david.blacher@ait.ac.at>
2019-08-22 09:59:49 +0200

    changed Makefile to be executable on Cluster

---
+++ create-MakeCluster.sh
@@ -0,0 +1,146 @@
+cd examples/showCases/cavity3d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../womersley
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../rectangularChannel3d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../poiseuille
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../carreauPoiseuille
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../boussinesqThermal3d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../rectangularChannel3d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../boussinesqThermal2d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../collidingBubbles3d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../movingWall
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../multiComponent3d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../multiComponent2d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../cavity2d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../externalFlowAroundObstacle
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../aneurysm
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../particlesInCone
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../heatedObjectInChannel
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../cylinder2d
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../generalExternalFlow
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../vofMultiPhase
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+
+cd ../../tutorial/permeability
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../tutorial_1
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../tutorial_2
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+
+cd ../../codesByTopic/coProcessor
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../transientStatistics
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../particlesInTube
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../marchingCube
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../complexNumbers
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../couplings
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../io
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../shanChenMultiPhase
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../copying
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../dynamicsInfo
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../navierStokesModels
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../bounceBack
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../dotList
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../externalMacroscopicVariables
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../asinariDynamics
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../scalarField
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../dataAnalysis
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../boundaryCondition
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../d3q13
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../linearAlgebra
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../userInput
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../multiBlock
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../smagorinskyModel
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../cutTriangularSurface
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
+cd ../reparallelize
+cp Makefile MakeCluster
+sed -i 's/mpicxx/\/usr\/lib64\/mpich\/bin\/mpicxx/g' MakeCluster
